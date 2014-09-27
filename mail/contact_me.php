32c8b3f11581ed8556c796c27f3101c1ba3656fc
Leonard Hoon <lh@leonardhoon.com>
2014-09-27 12:11:11 +1000

    Testing the mail function return to troubleshoot the broken contact form.

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -21,6 +21,6 @@ $email_subject = "Website Contact Form: $name";
 $email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nPhone: $phone\n\nMessage:\n$message";
 // /$headers = "From: lh@leonardhoon.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
 //$headers .= "Reply-To: $email_address";
-mail($to,$email_subject,$email_body, null, '-flh@leonardhoon.com');
-return true;
+return mail($to,$email_subject,$email_body, null, '-flh@leonardhoon.com');
+//return true;
 ?>
